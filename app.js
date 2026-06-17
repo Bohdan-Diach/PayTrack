8dabf257add788493bc60c28c0b38776bb7970aa
Bohdan-Diach <Bohdan.Diachenko@kname.edu.ua>
2026-06-17 08:54:50 +0300

    Register service worker for PWA support

--- app.js
+++ app.js
@@ -510,4 +510,13 @@ function initSettings() {
             }
         });
     }
+    
+}
+// ================= PWA: SERVICE WORKER =================
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => console.log('ServiceWorker зареєстровано успішно:', registration.scope))
+            .catch(err => console.log('Помилка реєстрації ServiceWorker:', err));
+    });
 }
